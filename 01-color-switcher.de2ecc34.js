64c5ab178bcbfabda0e8be5cabc04293a855de73
Perslay <124808493+Perslay@users.noreply.github.com>
2024-02-08 22:04:35 +0000

    Deploying to gh-pages from @ Perslay/goit-js-hw-09@c6d83a29c204190e0ae06290b53d6aff7400037c 🚀

---
+++ 01-color-switcher.de2ecc34.js
@@ -0,0 +1,2 @@
+!function(){"use strict";var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");n.disabled=!0;var o=null;function r(){return"#".concat(Math.floor(16777215*Math.random()).toString(16))}e.addEventListener("click",(function(){var a=r();t.style.backgroundColor=a,o=setInterval((function(){a=r(),t.style.backgroundColor=a}),1e3),n.disabled=!1,e.disabled=!0})),n.addEventListener("click",(function(){clearInterval(o),n.disabled=!0,e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.de2ecc34.js.map
