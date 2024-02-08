e9b0dd8b8cde3327aafc9d9955c40d67f92eaf95
Perslay <natalia.44.fedyk@gmail.com>
2024-02-08 22:24:22 +0100

    Task 1 done
    Start button triggers changing background color every seconds and the stop button stops it.

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+body = document.querySelector('body');
+startBtn = document.querySelector('button[data-start]');
+stopBtn = document.querySelector('button[data-stop]');
 
+stopBtn.disabled = true;
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStart() {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+
+  timerId = setInterval(() => {
+    color = getRandomHexColor();
+    body.style.backgroundColor = color;
+  }, 1000);
+
+  stopBtn.disabled = false;
+  startBtn.disabled = true;
+}
+
+function onStop() {
+  clearInterval(timerId);
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+}
+
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
