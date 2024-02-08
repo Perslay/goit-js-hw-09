9b70ae18e983050980781bf9ae3d484969cc9b75
Perslay <natalia.44.fedyk@gmail.com>
2024-02-08 22:55:49 +0100

    Task 1 fix
    body variable

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,32 +1,34 @@
-body = document.querySelector('body');
-startBtn = document.querySelector('button[data-start]');
-stopBtn = document.querySelector('button[data-stop]');
+document.addEventListener('DOMContentLoaded', function () {
+  let body = document.querySelector('body');
+  let startBtn = document.querySelector('button[data-start]');
+  let stopBtn = document.querySelector('button[data-stop]');
 
-stopBtn.disabled = true;
-let timerId = null;
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
-function onStart() {
-  let color = getRandomHexColor();
-  body.style.backgroundColor = color;
-
-  timerId = setInterval(() => {
-    color = getRandomHexColor();
-    body.style.backgroundColor = color;
-  }, 1000);
-
-  stopBtn.disabled = false;
-  startBtn.disabled = true;
-}
-
-function onStop() {
-  clearInterval(timerId);
   stopBtn.disabled = true;
-  startBtn.disabled = false;
-}
+  let timerId = null;
 
-startBtn.addEventListener('click', onStart);
-stopBtn.addEventListener('click', onStop);
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  function onStart() {
+    let color = getRandomHexColor();
+    body.style.backgroundColor = color;
+
+    timerId = setInterval(() => {
+      color = getRandomHexColor();
+      body.style.backgroundColor = color;
+    }, 1000);
+
+    stopBtn.disabled = false;
+    startBtn.disabled = true;
+  }
+
+  function onStop() {
+    clearInterval(timerId);
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+  }
+
+  startBtn.addEventListener('click', onStart);
+  stopBtn.addEventListener('click', onStop);
+});
