138f1f5590a20e5a0ca45249126468daf24cbdff
Perslay <natalia.44.fedyk@gmail.com>
2024-02-12 16:24:49 +0100

    Task 3 done
    Creating promises. First delay, other delays (steps) and amount of the promises are collected from inputs filled by user

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+'use strict';
+
+const form = document.querySelector('.form');
+const delay = document.querySelector("[name='delay']");
+const step = document.querySelector("[name='step']");
+const amount = document.querySelector("[name='amount']");
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    } else {
+      reject(`❌ Rejected promise ${position} in ${delay}ms`);
+    }
+  });
 }
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const totalAmount = parseInt(amount.value);
+  let createdPromises = 0;
+
+  const timerId = setInterval(() => {
+    if (createdPromises >= totalAmount) {
+      clearInterval(timerId);
+      return;
+    }
+    createPromise(createdPromises + 1, delay.value)
+      .then(resolved => console.log(resolved))
+      .catch(rejected => console.log(rejected));
+    createdPromises++;
+  }, step.value);
+});
